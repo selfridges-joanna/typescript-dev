8de2cc355c43342a273cc2e4fc36500365cf3ee3
selfridges-joanna <joanna.bak@selfridges.co.uk>
2023-02-16 09:49:18 +0000

    change the fn name

--- personal/movie-rental/tests/movie-rental.test.ts
+++ personal/movie-rental/tests/movie-rental.test.ts
@@ -1,6 +1,6 @@
 import {returnCustomerRentalStatement} from '../src/movie-rental'
 import { customers, movies } from '../src/data'
-import { ReturnAmount, FrequentRentPoints, PrintResult } from '../src/refactor'
+import { ReturnAmount, FrequentRentPoints, } from '../src/refactor'
 
 
 describe("Movie Rental should", () => {
@@ -80,7 +80,7 @@ describe("Movie Rental should", () => {
   describe("Movie Rental should", () => {
     it("returns the correct rental record statement", () => {
 
-      const customerStatement = PrintResult(customers[0], movies);
+      const customerStatement = returnCustomerRentalStatement(customers[0], movies);
       expect(customerStatement).toBe("Rental Record for Martin [ movie title: Ran, amount: 3.5] [ movie title: The Matrix, amount: 2] Amount owed is: 5.5. Customer earned: 2 frequent renter points.");
 
     })
